6311c61560197b7c13215f1c1190491f7eda1358
LBJANU <59941227+LBJANU@users.noreply.github.com>
2022-02-14 23:31:44 -0500

    Workable Copy of Website Original

---
+++ index.js
@@ -0,0 +1,22 @@
+
+/*
+* anime.js v3.2.1
+* (c) 2020 Julian Garnier
+* Released under the MIT license
+* animejs.com
+*/
+
+
+function quest_129736_features () {
+    let actionAnims = {};
+    let scrollTriggersFired = [];
+    let fireScrollOffTriggers = [];
+    let questVars = {};
+  let actualInput = null;let actualCheckbox = null;
+    let checkDiv = null;
+  
+  };
+  
+      document.addEventListener("DOMContentLoaded", quest_129736_features);
+    0
+0
